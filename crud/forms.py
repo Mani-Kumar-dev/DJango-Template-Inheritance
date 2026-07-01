3f96cebb7ad02f2858a9cd9f81013d031f790497
Mani Kumar <142436903+Mani-Kumar-dev@users.noreply.github.com>
2026-07-01 13:39:52 +0530

    Employee list and edit

--- crud/forms.py
+++ crud/forms.py
@@ -5,3 +5,24 @@ class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = '__all__'
+        
+        widgets = {
+            'emp_id' : forms.NumberInput(attrs={
+                'class':'form-control',
+                'placeholder': 'Emp id'
+            }),
+            'emp_name' : forms.TextInput(attrs={
+                'class':'form-control',
+                'placeholder':'Emp Name'
+                
+            }),
+            'emp_email' : forms.EmailInput(attrs={
+                'class':'form-control',
+                'placeholder':'Emp Email'
+            }),
+            'emp_address' : forms.TextInput(attrs={
+                'class':'form-control',
+                'placeholder':'Emp Address'
+            })
+            
+        }
